77ca566e5e8932094034f181bc2cbea5ebf6a396
Kamal Mustafa <kamal@xoxzo.com>
2017-10-05 16:56:57 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,12 @@
+## Quickstart
+
+You need `pipenv` installed.
+
+```
+pipenv install
+TG_CHAT_ID=xxx TG_BOT_TOKEN=xxx SLACK_API_TOKEN=xxx pipenv run python slacktg.py
+```
+
+## Todos
+
+* Add on/off toggle. Doesn't make any sense to do the forwarding if I'm at my desk.
---
+++ slacktg.py
@@ -0,0 +1,106 @@
+
+"""
+Copyright (c) 2017-present Kamal Bin Mustafa <k4ml@github.io>
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
+
+import os
+import time
+import logging
+import threading
+
+import telegram
+
+from slackclient import SlackClient
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+sc = SlackClient(slack_token)
+
+tg_token = os.environ['TG_BOT_TOKEN']
+bot = telegram.Bot(token=tg_token)
+chat_id = int(os.environ['TG_CHAT_ID'])
+
+def get_channel_info(channel_id):
+    resp = sc.api_call('channels.info', channel=channel_id)
+    if resp['ok']:
+        return resp['channel']
+    return None
+
+def listen_slack():
+    if sc.rtm_connect():
+        while True:
+            messages = sc.rtm_read()
+            for msg in messages:
+                if msg['type'] != 'message':
+                    continue
+                if 'subtype' in msg and msg['subtype'] == 'bot_message':
+                    continue
+                print(msg)
+                resp = sc.api_call('users.info', user=msg['user'])
+                if resp['ok']:
+                    user = resp['user']
+                    channel = get_channel_info(msg['channel']) or 'N/A'
+                    print(user['name'], msg['text'])
+                    ret = bot.sendMessage(chat_id=chat_id, text='#%s:%s> %s' % (channel['name'], user['name'], msg['text']))
+            time.sleep(3)
+    else:
+        print("Connection Failed")
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+def listen_telegram():
+    offset = -1
+    update = None
+    channel = '#integrationsandbox'
+    while True:
+        try:
+            updates = bot.get_updates(offset)
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+            continue
+        for update in updates:
+            if update.message.chat.id not in [chat_id]:
+                print(update.message.chat.id, chat_id)
+                update.message.reply_text('invalid user')
+                continue
+            reply = update.message.reply_to_message
+            if reply:
+                channel_part = reply.text.split(':')[0]
+                if channel_part.strip().startswith('#'):
+                    channel = channel_part
+            sc.api_call('chat.postMessage', channel=channel, text='From telegram: %s' % update.message.text)
+            print(update.message.text)
+            print(update.message.reply_to_message)
+            if 'posted to slack' not in update.message.text:
+                update.message.reply_text('posted to slack')
+
+        if update:
+            offset = update.update_id + 1
+        time.sleep(2)
+
+if __name__ == '__main__':
+    tg_thread = threading.Thread(target=listen_telegram)
+    slack_thread = threading.Thread(target=listen_slack)
+    tg_thread.start()
+    slack_thread.start()
